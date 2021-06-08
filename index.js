5a3ab342830b8ef9603b474e1be71cb92f2b706e
GaiaChild <sd20000507@gmail.com>
2021-06-08 10:08:55 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+* Primary file for the API
+*
+*/
+
+// Dependencies
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+
+// Declare the app
+var app = {};
+
+// Init function
+app.init = function(){
+  // Start the server
+  server.init();
+
+  // Start the workers;
+  workers.init();
+};
+
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
